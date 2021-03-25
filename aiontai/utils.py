e73a25f7806588d9d76c2cf25f62848fe04447e3
AlekseyZz <TheHentWill@gmail.com>
2021-03-25 23:09:01 +0200

    Write a function for checking structure

---
+++ aiontai/utils.py
@@ -0,0 +1,14 @@
+from typing import Dict, Any
+from schema import SchemaError, Schema
+from .config import config
+from . import errors
+
+
+def is_valid_structure(schema: Schema, json: dict) -> bool:
+    try:
+        schema.validate(json)
+        return True
+    except SchemaError:
+        raise errors.IsNotValidStructure(
+            "You cant build a class from JSON, because it havent valid JSON structure."
+        )
