0b9776ead91420edf6cbe1cc855bca3cb48a8cec
AlekseyZz <TheHentWill@gmail.com>
2021-03-28 21:55:45 +0300

    Write a main API class and tests for that

---
+++ aiontai/__init__.py
@@ -0,0 +1,135 @@
+__all__ = ["API"]
+
+from typing import List
+from aiontai import api, utils, models
+
+nhentai_api = api.NHentaiAPI()
+
+
+class API:
+    @staticmethod
+    async def get_doujin(id: int) -> models.Doujin:
+        """Method for getting doujin by id.
+        Args:
+            :id int: Doujin's id, which we get.
+
+        Returns:
+            JSON of doujin.
+
+        Raises:
+            DoujinDoesNotExist if doujin was not found.
+
+        Usage:
+            >>> api = NHentaiAPI()
+            >>> await api.get_doujin(1)
+            Doujin(...)
+        """
+        response = await nhentai_api.get_doujin(id)
+        json = await utils.make_doujin_json(response)
+
+        return models.Doujin.from_json(json)
+
+    @staticmethod
+    async def is_exist(id: int) -> bool:
+        """Method for checking does doujin exist.
+        Args:
+            :id int: Doujin's id, which we check.
+
+        Returns:
+            True if doujin is exist, False if doujin is not exist.
+
+        Usage:
+            >>> api = NHentaiAPI()
+            >>> await api.is_exist(1)
+            True
+        """
+        response = await nhentai_api.is_exist(id)
+
+        return response
+
+    @staticmethod
+    async def get_random_doujin() -> models.Doujin:
+        """Method for getting random doujin.
+        Returns:
+            JSON of random doujin.
+
+        Usage:
+            >>> api = NHentaiAPI()
+            >>> await api.random_doujin()
+            Doujin(...)
+        """
+        response = await nhentai_api.get_random_doujin()
+        json = await utils.make_doujin_json(response)
+
+        return models.Doujin.from_json(json)
+
+    @staticmethod
+    async def search(query: str, *, page: int = 1, sort_by: str = "date") -> List[models.Doujin]:
+        """Method for search doujins.
+        Args:
+            :query str: Query for search doujins.
+            :page int: Page, from which we return results.
+            :sort_by str: Sort for search.
+
+        Returns:
+            List of doujins JSON
+
+        Raises:
+            IsNotValidSort if sort is not a member of SortOptions.
+            WrongPage if page less than 1.
+
+        Usage:
+            >>> api = NHentaiAPI()
+            >>> await api.search("anime", page=2, sort_by="popular")
+            [Doujin(...), ...]
+        """
+        response = await nhentai_api.search(query, page, sort_by)
+        results = [await utils.make_doujin_json(json) for json in response]
+        return [models.Doujin.from_json(json) for json in results]
+
+    @staticmethod
+    async def search_by_tag(tag: int, *, page: int = 1, sort_by: str = "date") -> List[models.Doujin]:
+        """Method for search doujins by tag.
+        Args:
+            :tag id: Tag for search doujins.
+            :page int: Page, from which we return results.
+            :sort_by str: Sort for search.
+
+        Returns:
+            List of doujins JSON
+
+        Raises:
+            IsNotValidSort if sort is not a member of SortOptions.
+            WrongPage if page less than 1 or page has no content.
+            WrongTag if tag with given id does not exist.
+
+        Usage:
+            >>> api = NHentaiAPI()
+            >>> awaitapi.search_by_tag(1, page=2, sort_by="popular")
+            [Doujin(...), ...]
+        """
+        response = await nhentai_api.search_by_tag(tag, page, sort_by)
+        print(response)
+        results = [await utils.make_doujin_json(json) for json in response]
+        return [models.Doujin.from_json(json) for json in results]
+
+    @staticmethod
+    async def get_homepage_doujins(*, page: int = 1) -> List[models.Doujin]:
+        """Method for getting doujins from.
+        Args:
+            :page int: Page, from which we get doujins.
+
+        Returns:
+            List of doujins JSON
+
+        Raises:
+            WrongPage if page less than 1 or page has no content.
+
+        Usage:
+            >>> api = NHentaiAPI()
+            >>> await api.get_homepage_doujins(1)
+            [Doujin(...), ...]
+        """
+        response = await nhentai_api.get_homepage_doujins(page)
+        results = [await utils.make_doujin_json(json) for json in response]
+        return [models.Doujin.from_json(json) for json in results]
---
+++ tests/api_test.py
@@ -0,0 +1,81 @@
+import os
+import site
+import asyncio
+site.addsitedir(os.getcwd())
+
+import pytest
+import aiontai
+from aiontai import errors
+
+
+@pytest.mark.asyncio
+async def test_api_get_doujin():
+    api = aiontai.API()
+    doujin = await api.get_doujin(1)
+    assert (
+           doujin.id 
+           and doujin.title
+           and doujin.media_id
+           and doujin.cover
+           and doujin.thumbnail
+           and doujin.tags
+           and doujin.pages
+           and doujin.pages_count
+           )
+
+
+@pytest.mark.asyncio
+async def test_api_get_doujin_wrong():
+    api = aiontai.API()
+    with pytest.raises(errors.DoujinDoesNotExist):
+        await api.get_doujin(0)
+
+
+@pytest.mark.asyncio
+async def test_api_is_exist_true():
+    api = aiontai.API()
+    is_exist_doujin = await api.is_exist(1)
+    assert is_exist_doujin
+
+
+@pytest.mark.asyncio
+async def test_api_is_exist_false():
+    api = aiontai.API()
+    is_exist_doujin = await api.is_exist(0)
+    assert not is_exist_doujin
+
+
+@pytest.mark.asyncio
+async def test_api_get_random_doujin():
+    api = aiontai.API()
+    doujin = await api.get_random_doujin()
+    assert (
+           doujin.id 
+           and doujin.title
+           and doujin.media_id
+           and doujin.cover
+           and doujin.thumbnail
+           and doujin.tags
+           and doujin.pages
+           and doujin.pages_count
+           )
+
+
+@pytest.mark.asyncio
+async def test_api_search():
+    api = aiontai.API()
+    doujins = await api.search("anime", page=2, sort_by="popular")
+    assert doujins
+
+
+@pytest.mark.asyncio
+async def test_api_search_by_tag():
+    api = aiontai.API()
+    await api.search_by_tag(1, sort_by="popular")
+
+
+@pytest.mark.asyncio
+async def test_api_homepage_doujins():
+    api = aiontai.API()
+    doujins = await api.get_homepage_doujins(page=2)
+    assert doujins
